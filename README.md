ae69d04707a1af83cafc72e104f14994bbfa2a91
Agyky <85668871+Vladimir-Agyky@users.noreply.github.com>
2024-05-11 11:54:24 +0900

    Third push
    Fix no body content issue

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
-# Google Smtp / Gmail Mailbox Crawling
+# Google Smtp Sender Flask
+![SCR-20240406-kwuz](https://github.com/Vladimir-Agyky/GoogleSmtpSender/assets/85668871/6be4d8ac-dc99-4762-9b00-088d6f31b7b0)
 
 # Before Installation
-Go to gmail
-<img width="1385" alt="SCR-20240217-kwhu" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/6d72a7da-d8c1-4983-b266-5fd00b59f95e">
-Open setting
-<img width="1386" alt="SCR-20240217-kwqn" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/6b4fcbf9-c43a-46bb-8bb4-678f1de4ddfc">
-Go to POP/IMAP
-<img width="1389" alt="SCR-20240217-kwvs" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/fa8fabb5-6a18-4e20-966b-e0202541cc7a">
-Enable it all
-<img width="1383" alt="SCR-20240217-kwyu" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/4dab3dc5-8288-4cf2-a7cb-e50b25110dba">
-Go to google account -> Second auth
-<img width="1387" alt="SCR-20240217-kxly" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/bcc071fb-6cc4-41e1-b320-12da7afd97ad">
-App Password, You can set app name randomly
-<img width="1384" alt="SCR-20240217-kxsy" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/0485fa84-f872-43e8-832d-36ff9e7ec18d">
-
-# Config
-<img width="1103" alt="SCR-20240217-kyxz" src="https://github.com/Vladimir-Agyky/GoogleSmtp/assets/85668871/1e5e0aae-5a12-435f-87a1-e9949489cba1">
-
+You need to enable smtp/imap in gmail <br>
+Tutorial : https://github.com/Vladimir-Agyky/GoogleSmtp
 
 # installation
 ```
@@ -28,3 +15,15 @@ pip3 install email
 ```
 python3 main.py
 ```
+
+# How to use?
+<img width="172" alt="SCR-20240406-lbhd" src="https://github.com/Vladimir-Agyky/GoogleSmtpSender/assets/85668871/2d644f38-9380-4bec-98a2-2560fbd8d50d"> <br>
+Site ID : Whatever you want <br>
+SITE Pw : whatever you want <br>
+Smtp email : your google email <br>
+smtp password : you google app password <br>
+ㄴ qhgf qkdn eitj gkeg <- Not Correct <br>
+ㄴ qhgfqkdneitjgkeg <- Correct :) <br>
+Information : https://github.com/Vladimir-Agyky/GoogleSmtp <br>
+
+# This project is sequel project by https://github.com/Vladimir-Agyky/GoogleSmtp
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
+import smtplib
+import imaplib
+from email.parser import BytesParser
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import html
 
 app = Flask(__name__)
 app.secret_key = 'SaltySalt09@'  # 암호화를 위한 시크릿 키 설정
@@ -28,24 +31,31 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    error_message = None  # 오류 메시지를 저장할 변수 초기화
     if request.method == 'POST':
         site_id = request.form['site_id']
-        site_password = generate_password_hash(request.form['site_password'])
-        smtp_email = request.form['smtp_email']
-        smtp_password = request.form['smtp_password']
-        user = User(site_id=site_id, site_password=site_password, smtp_email=smtp_email, smtp_password=smtp_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registration successful! Please log in.')
-        return redirect(url_for('login'))
-    return render_template('register.html')
+        if len(site_id) < 4:
+            error_message = 'Username must be at least 4 characters long.'
+        elif User.query.filter_by(site_id=site_id).first():
+            error_message = 'Username already exists.'
+        else:
+            site_password = generate_password_hash(request.form['site_password'])
+            smtp_email = request.form['smtp_email']
+            smtp_password = request.form['smtp_password']
+            user = User(site_id=site_id, site_password=site_password, smtp_email=smtp_email, smtp_password=smtp_password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Registration successful! Please log in.')
+            return redirect(url_for('login'))
+    return render_template('register.html', error_message=error_message)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error_message = None  # 오류 메시지를 저장할 변수 초기화
     if request.method == 'POST':
         site_id = request.form['site_id']
         user = User.query.filter_by(site_id=site_id).first()
@@ -54,8 +64,9 @@ def login():
             flash('Login successful!')
             return redirect(url_for('send_email'))
         else:
-            flash('Login unsuccessful. Please check your credentials.')
-    return render_template('login.html')
+            error_message = 'Incorrect username or password. Please try again.'
+    return render_template('login.html', error_message=error_message)
+
 
 @app.route('/logout')
 @login_required
@@ -67,14 +78,27 @@ def logout():
 @app.route('/send_email', methods=['GET', 'POST'])
 @login_required
 def send_email():
+    messages = []  # 이 부분에서 messages 변수를 먼저 정의합니다.
     if request.method == 'POST':
         receiver_email = request.form['receiver_email']
         subject = request.form['subject']
         message = request.form['message']
+        anonymous = request.form.get('anonymous')  # Check if anonymous checkbox is checked
 
         # 이메일 내용
         msg = MIMEMultipart()
-        msg['From'] = current_user.smtp_email
+        
+        # 익명으로 보내기 체크되었는지 확인
+        if anonymous:
+            # 익명으로 보내기일 경우 미리 저장된 SMTP 자격 증명 사용
+            smtp_email = ""  # 미리 저장된 SMTP 이메일
+            smtp_password = ""  # 미리 저장된 SMTP 비밀번호
+        else:
+            # 익명으로 보내지 않을 경우 현재 사용자의 SMTP 자격 증명 사용
+            smtp_email = current_user.smtp_email
+            smtp_password = current_user.smtp_password
+
+        msg['From'] = smtp_email
         msg['To'] = receiver_email
         msg['Subject'] = subject
         msg.attach(MIMEText(message, 'plain'))
@@ -82,17 +106,92 @@ def send_email():
         # SMTP 서버 설정
         smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
         smtp_server.starttls()
-        smtp_server.login(current_user.smtp_email, current_user.smtp_password)
+        smtp_server.login(smtp_email, smtp_password)
+        imap_server = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap_server.login(smtp_email, smtp_password)
+        imap_server.select('inbox')
+
+        _, message_numbers = imap_server.search(None, 'ALL')
+
+        for num in message_numbers[0].split():
+            _, data = imap_server.fetch(num, '(RFC822)')
+            msg = BytesParser().parsebytes(data[0][1])
+            body = msg.get_payload(decode=True).decode('utf-8') if msg.get_payload(decode=True) else "" # 본문 가져오기
+            messages.append({
+                'subject': msg['subject'],
+                'from': msg['from'],
+                'date': msg['date'],
+                'body': body
+            })
+
+        # IMAP 서버 연결 종료
+        imap_server.close()
+        imap_server.logout()
 
         # 이메일 전송
         smtp_server.send_message(msg)
         smtp_server.quit()
 
-        flash('Email sent successfully!')
+        flash('이메일 전송 성공!')
         return redirect(url_for('send_email'))
-    return render_template('send_email.html')
+    return render_template('send_email.html', messages=messages)
 
-if __name__ == '__main__':  
-    with app.app_context():
-        db.create_all()
+
+@app.route('/inbox')
+@login_required
+def inbox():
+    # SMTP 이메일 가져오기
+    smtp_email = current_user.smtp_email
+    smtp_password = current_user.smtp_password
+
+    imap_server = imaplib.IMAP4_SSL('imap.gmail.com')
+    imap_server.login(smtp_email, smtp_password)
+    imap_server.select('inbox')
+
+    _, message_numbers = imap_server.search(None, 'ALL')
+    messages = []
+
+    for num in message_numbers[0].split():
+        _, data = imap_server.fetch(num, '(RFC822)')
+        msg = BytesParser().parsebytes(data[0][1])
+        body = None
+        
+        # 이메일이 복합 형식인지 확인
+        if msg.is_multipart():
+            for part in msg.walk():
+                content_type = part.get_content_type()
+                if content_type == "text/plain":
+                    # 텍스트 본문을 가져옴
+                    body = part.get_payload(decode=True).decode('utf-8')
+                    break  # 텍스트 본문을 찾으면 반복문 종료
+                elif content_type == "text/html":
+                    # HTML 본문도 있는 경우 텍스트 본문으로 처리
+                    body = part.get_payload(decode=True).decode('utf-8')
+        else:
+            # 단일 형식의 본문인 경우 바로 가져옴
+            body = msg.get_payload(decode=True).decode('utf-8') if msg.get_payload(decode=True) else ""
+
+    # 본문이 존재하는지 확인하여 처리
+    if body:
+        messages.append({
+            'subject': msg['subject'],
+            'from': msg['from'],
+            'date': msg['date'],
+            'body': body
+        })
+    else:
+        messages.append({
+            'subject': msg['subject'],
+            'from': msg['from'],
+            'date': msg['date'],
+            'body': "No Body Contents"
+        })
+
+
+    # IMAP 서버 연결 종료
+    imap_server.close()
+    imap_server.logout()
+    return render_template('inbox.html', messages=messages)
+
+if __name__ == '__main__':
     app.run(debug=True)
